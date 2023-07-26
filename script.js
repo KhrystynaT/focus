560ee050a14d779a481a5e4c5c49face1728fa11
Khrystyna <khrystyna.tyvoniuk@gmail.com>
2023-07-26 03:47:38 +0200

    changed data to monthly

--- script.js
+++ script.js
@@ -1,34 +1,160 @@
-// Customized schedule data based on your requirements
+//monthly data
 const scheduleData = {
-  Monday: [
+  "August 1": [
     "Morning: Daily Full-Stack Development Practice and GitHub Commit (1 hour)",
-    "Afternoon: University Discussion Forum for English (1 hour)",
+    "Afternoon: University Course 1 - Assignment (1 hour)",
+    "Evening: Python Course 1, Module 1 (1 hour)",
   ],
-  Tuesday: [
+  "August 2": [
     "Morning: Daily Full-Stack Development Practice and GitHub Commit (1 hour)",
-    "Afternoon: University Discussion Forum for Databases (2 hours)",
+    "Afternoon: University Course 2 - Discussion Forum (1 hour)",
+    "Evening: Python Course 1, Module 2 (1 hour)",
   ],
-  Wednesday: [
+  "August 3": [
     "Morning: Daily Full-Stack Development Practice and GitHub Commit (1 hour)",
-    "Afternoon: University Databases Programming Assignment (2-3 hours)",
+    "Afternoon: University Course 1 - Learning Journal (1 hour)",
+    "Evening: Python Course 1, Module 3 (1 hour)",
   ],
-  Thursday: [
+  "August 4": [
     "Morning: Daily Full-Stack Development Practice and GitHub Commit (1 hour)",
-    "Afternoon: University English Writing Assignment (1 hour)",
+    "Afternoon: University Course 2 - Programming Assignment (2 hours)",
+    "Evening: Python Course 1, Module 4 (1 hour)",
   ],
-  Friday: [
+  "August 5": [
     "Morning: Daily Full-Stack Development Practice and GitHub Commit (1 hour)",
-    "Afternoon: University Learning Journals for Both Courses (2-3 hours)",
+    "Afternoon: University Course 1 - Discussion Forum (1 hour)",
+    "Evening: Python Course 1, Module 5 (1 hour)",
   ],
-  Saturday: [
+  "August 6": [
     "Morning: Daily Full-Stack Development Practice and GitHub Commit (1 hour)",
-    "Afternoon: Python Course 1, Module 1 (1 hour)",
+    "Afternoon: University Course 2 - Learning Journal (1 hour)",
+    "Evening: Python Course 1, Module 6 (1 hour)",
   ],
-  Sunday: [
+  "August 7": [
     "Morning: Daily Full-Stack Development Practice and GitHub Commit (1 hour)",
-    "Afternoon: Python Course 1, Module 2 (1 hour)",
+    "Afternoon: University Course 1 - Programming Assignment (2-3 hours)",
+    "Evening: Python Course 1, Module 7 (1 hour)",
   ],
-  // Continue adding activities for the remaining days...
+  "August 8": [
+    "Morning: Daily Full-Stack Development Practice and GitHub Commit (1 hour)",
+    "Afternoon: University Course 2 - Assignment (1 hour)",
+    "Evening: Python Course 1, Module 8 (1 hour)",
+  ],
+  "August 9": [
+    "Morning: Daily Full-Stack Development Practice and GitHub Commit (1 hour)",
+    "Afternoon: University Course 1 - Discussion Forum (1 hour)",
+    "Evening: Python Course 1, Module 9 (1 hour)",
+  ],
+  "August 10": [
+    "Morning: Daily Full-Stack Development Practice and GitHub Commit (1 hour)",
+    "Afternoon: University Course 2 - Learning Journal (1 hour)",
+    "Evening: Python Course 1, Module 10 (1 hour)",
+  ],
+  "August 11": [
+    "Morning: Daily Full-Stack Development Practice and GitHub Commit (1 hour)",
+    "Afternoon: University Course 1 - Programming Assignment (2-3 hours)",
+    "Evening: Python Course 1, Module 11 (1 hour)",
+  ],
+  "August 12": [
+    "Morning: Daily Full-Stack Development Practice and GitHub Commit (1 hour)",
+    "Afternoon: University Course 2 - Discussion Forum (1 hour)",
+    "Evening: Python Course 1, Module 12 (1 hour)",
+  ],
+  "August 13": [
+    "Morning: Daily Full-Stack Development Practice and GitHub Commit (1 hour)",
+    "Afternoon: University Course 1 - Learning Journal (1 hour)",
+    "Evening: Python Course 1, Module 13 (1 hour)",
+  ],
+  "August 14": [
+    "Morning: Daily Full-Stack Development Practice and GitHub Commit (1 hour)",
+    "Afternoon: University Course 2 - Programming Assignment (2 hours)",
+    "Evening: Python Course 1, Module 14 (1 hour)",
+  ],
+  "August 15": [
+    "Morning: Daily Full-Stack Development Practice and GitHub Commit (1 hour)",
+    "Afternoon: University Course 1 - Discussion Forum (1 hour)",
+    "Evening: Python Course 1, Module 15 (1 hour)",
+  ],
+  "August 16": [
+    "Morning: Daily Full-Stack Development Practice and GitHub Commit (1 hour)",
+    "Afternoon: University Course 2 - Learning Journal (1 hour)",
+    "Evening: Python Course 1, Module 16 (1 hour)",
+  ],
+  "August 17": [
+    "Morning: Daily Full-Stack Development Practice and GitHub Commit (1 hour)",
+    "Afternoon: University Course 1 - Programming Assignment (2-3 hours)",
+    "Evening: Python Course 1, Module 17 (1 hour)",
+  ],
+  "August 18": [
+    "Morning: Daily Full-Stack Development Practice and GitHub Commit (1 hour)",
+    "Afternoon: University Course 2 - Assignment (1 hour)",
+    "Evening: Python Course 1, Module 18 (1 hour)",
+  ],
+  "August 19": [
+    "Morning: Daily Full-Stack Development Practice and GitHub Commit (1 hour)",
+    "Afternoon: University Course 1 - Discussion Forum (1 hour)",
+    "Evening: Python Course 1, Module 19 (1 hour)",
+  ],
+  "August 20": [
+    "Morning: Daily Full-Stack Development Practice and GitHub Commit (1 hour)",
+    "Afternoon: University Course 2 - Learning Journal (1 hour)",
+    "Evening: Python Course 1, Module 20 (1 hour)",
+  ],
+  "August 21": [
+    "Morning: Daily Full-Stack Development Practice and GitHub Commit (1 hour)",
+    "Afternoon: University Course 1 - Programming Assignment (2-3 hours)",
+    "Evening: Python Course 1, Module 21 (1 hour)",
+  ],
+  "August 22": [
+    "Morning: Daily Full-Stack Development Practice and GitHub Commit (1 hour)",
+    "Afternoon: University Course 2 - Discussion Forum (1 hour)",
+    "Evening: Python Course 1, Module 22 (1 hour)",
+  ],
+  "August 23": [
+    "Morning: Daily Full-Stack Development Practice and GitHub Commit (1 hour)",
+    "Afternoon: University Course 1 - Learning Journal (1 hour)",
+    "Evening: Python Course 1, Module 23 (1 hour)",
+  ],
+  "August 24": [
+    "Morning: Daily Full-Stack Development Practice and GitHub Commit (1 hour)",
+    "Afternoon: University Course 2 - Programming Assignment (2 hours)",
+    "Evening: Python Course 1, Module 24 (1 hour)",
+  ],
+  "August 25": [
+    "Morning: Daily Full-Stack Development Practice and GitHub Commit (1 hour)",
+    "Afternoon: University Course 1 - Discussion Forum (1 hour)",
+    "Evening: Python Course 1, Module 25 (1 hour)",
+  ],
+  "August 26": [
+    "Morning: Daily Full-Stack Development Practice and GitHub Commit (1 hour)",
+    "Afternoon: University Course 2 - Learning Journal (1 hour)",
+    "Evening: Python Course 1, Module 26 (1 hour)",
+  ],
+  "August 27": [
+    "Morning: Daily Full-Stack Development Practice and GitHub Commit (1 hour)",
+    "Afternoon: University Course 1 - Programming Assignment (2-3 hours)",
+    "Evening: Python Course 1, Module 27 (1 hour)",
+  ],
+  "August 28": [
+    "Morning: Daily Full-Stack Development Practice and GitHub Commit (1 hour)",
+    "Afternoon: University Course 2 - Assignment (1 hour)",
+    "Evening: Python Course 1, Module 28 (1 hour)",
+  ],
+  "August 29": [
+    "Morning: Daily Full-Stack Development Practice and GitHub Commit (1 hour)",
+    "Afternoon: University Course 1 - Discussion Forum (1 hour)",
+    "Evening: Python Course 1, Module 29 (1 hour)",
+  ],
+  "August 30": [
+    "Morning: Daily Full-Stack Development Practice and GitHub Commit (1 hour)",
+    "Afternoon: University Course 2 - Learning Journal (1 hour)",
+    "Evening: Python Course 1, Module 30 (1 hour)",
+  ],
+  "August 31": [
+    "Morning: Daily Full-Stack Development Practice and GitHub Commit (1 hour)",
+    "Afternoon: University Course 1 - Programming Assignment (2-3 hours)",
+    "Evening: Python Course 1, Final Review and Summary (1 hour)",
+  ],
 };
 
 // Function to display the schedule
